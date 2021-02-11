e2a4828c215c8cce002759412ce9bd82990c3711
Dries Augustyns <r0781133@student.thomasmore.be>
2021-02-11 13:59:26 +0100

    Added new extract to exports

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { convert } from "./lib/convert";
 import { addSymbol } from "./lib/addSymbol";
+import { extract } from "./lib/extract";
 
-export { convert, addSymbol };
+export { convert, addSymbol, extract };
